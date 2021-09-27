9a6e00c42b1e3d2bf80b07b25f5f3bbd03c1aedf
fabio andres balanta <fabioannypro47@gmail.com>
2021-09-26 20:43:44 -0500

    edicion enviar.php

--- Sitio_web_restaurantev2/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion = mysql_connect("localhost","root","","bdbar");
-if(!$conexion) {
-    echo "Error al conectar a la base de datos";
-}
-else {
-    echo "Conectado a la base de datos";
-}
--- Sitio_web_restaurantev2/enviar.php
+++ Sitio_web_restaurantev2/enviar.php
@@ -1,21 +1,21 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+$asunto = $_POST['asunto'];
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$servicio = $_POST['servicio'];
 
-$header = 'From: ' . $mail . " \r\n";
+$header = 'From: ' . $correo . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message .= "Su e-mail es: " . $correo . " \r\n";
+$message .= "Teléfono de contacto: " . $servicio . " \r\n";
+$message .= "Mensaje: " . $_POST['servicio'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'escribeaquitucorreo@hotmail.com';
+$para = 'andres.tateno@gmail.com';
 $asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
 
 mail($para, $asunto, utf8_decode($message), $header);

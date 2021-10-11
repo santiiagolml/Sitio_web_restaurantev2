57bb4e240871e98deeb35034bb8c4a0a506139fd
alvarosanchezc <sanchezcameloalvaro@gmail.com>
2021-10-11 16:17:39 -0500

    CambiosEnCarrito
    Se agregan cambios en el carrito de compras

--- Sitio_web_restaurantev2/js/app.js
+++ Sitio_web_restaurantev2/js/app.js
@@ -4,14 +4,15 @@ const footer = document.getElementById('footer');
 const templateCard =document.getElementById('template-card').content
 const templateFooter =document.getElementById('template-footer').content
 const templateCarrito =document.getElementById('template-carrito').content
+const pintarImagenesDeCarrito = document.getElementById('cards');
 const fragment =document.createDocumentFragment();
 let carrito = {};
-
 
 document.addEventListener('DOMContentLoaded', () => {
 	fetchData()
 	if (localStorage.getItem('carrito')){
 		carrito = JSON.parse(localStorage.getItem('carrito'));
+		console.log(carrito);
 		pintarCarrito();
 	}
 })
@@ -43,6 +44,7 @@ const pintarCards = data =>{
 		templateCard.querySelector('p').textContent = producto.precio;
 		templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl);
 		templateCard.querySelector('.btn-dark').dataset.id = producto.id;
+	//	console.log(producto.thumbnailUrl);
 		const clone = templateCard.cloneNode(true);
 
 		//para evitar reflow se usa el fragment
@@ -50,6 +52,8 @@ const pintarCards = data =>{
 	} )
 	cards.appendChild(fragment)
 }
+
+
 
 // Detecta los clics que se dan dentro del template
 const addCarrito = e => {
@@ -65,35 +69,49 @@ const setCarrito = objeto => {
 	const producto = {
 		id: objeto.querySelector('.btn-dark').dataset.id,
 		title: objeto.querySelector('h5').textContent,
+		imagen:objeto.querySelector('img').getAttribute("src"),
 		precio: objeto.querySelector('p').textContent,
 		cantidad: 1
 	}
+	console.log(producto);
+
+	/*
+	Object.values(producto).forEach(elemento => {
+		console.log(elemento.id);
+	})
+	*/
 	if(carrito.hasOwnProperty(producto.id)){
 		producto.cantidad = carrito[producto.id].cantidad + 1;
 	}
 	carrito[producto.id] = {...producto}
 	pintarCarrito();
+	//console.log(carrito);
 }
-
+//Se muestra en el carrito 
 const pintarCarrito = () => {
 	//console.log(carrito);
-	items.innerHTML= ''
+	items.innerHTML= '<img src= ${producto.imagen.currentSrc}  alt="">'
 	Object.values(carrito).forEach(producto => {
 		templateCarrito.querySelector('th').textContent = producto.id;
 		templateCarrito.querySelectorAll('td')[0].textContent = producto.title;
 		templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad;
+
+		templateCarrito.querySelector('img').setAttribute("src", producto.imagen.currentSrc);
 		templateCarrito.querySelector('.btn-info').dataset.id = producto.id;
 		templateCarrito.querySelector('.btn-danger').dataset.id = producto.id;
 		templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio;
+		console.log(templateCarrito.querySelector('img').getAttribute("src"));
+
 		const clone = templateCarrito.cloneNode(true);
 		fragment.appendChild(clone);
 	})
 	items.appendChild(fragment)
-
-	pintarFooter()
+pintarFooter()
 	//se guardan los elementos en el localstorage
+	console.log(JSON.stringify(carrito))
 	localStorage.setItem('carrito', JSON.stringify(carrito));
-}   
+
+}
 
 const pintarFooter = () => {
 	footer.innerHTML = '';
@@ -104,7 +122,7 @@ const pintarFooter = () => {
 	}
 	const nCantidad = Object.values(carrito).reduce((acc, {cantidad})=> acc + cantidad,0);
 	const nPrecio = Object.values(carrito).reduce((acc, {cantidad,precio})=> acc + cantidad * precio,0);
-	console.log(nPrecio);
+	
 
 	templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
 	templateFooter.querySelector('span').textContent = nPrecio;
@@ -115,6 +133,12 @@ const pintarFooter = () => {
 
 	const btnVaciar = document.getElementById('vaciar-carrito');
 	btnVaciar.addEventListener('click', () => {
+		carrito = {};
+		pintarCarrito();
+	})
+
+	const btnConfirmarPedido = document.getElementById('confirmar-pedido');
+	btnConfirmarPedido.addEventListener('click', () => {
 		carrito = {};
 		pintarCarrito();
 	})
@@ -141,4 +165,5 @@ const btnAccion = e => {
 	}
 
 	e.stopPropagation();
+
 }
